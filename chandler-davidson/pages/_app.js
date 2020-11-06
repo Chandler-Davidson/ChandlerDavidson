603b7b71e702025e07358c42f37ba53bcef7cffe
Chandler Davidson <chandlermdavidson@gmail.com>
2020-11-06 13:28:38 -0600

    Update styling

--- chandler-davidson/pages/_app.js
+++ chandler-davidson/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/.com.styles.css';
+import '../styles/.dev.styles.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
