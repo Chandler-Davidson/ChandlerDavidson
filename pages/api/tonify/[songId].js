1bb2d649da3e0fb7c8447a2512b43d6e978a612e
Chandler Davidson <chandlermdavidson@gmail.com>
2021-05-25 10:11:03 -0500

    Fix relative path

--- pages/api/tonify/[songId].js
+++ pages/api/tonify/[songId].js
@@ -1,4 +1,4 @@
-const db = require('../../../util/firestore').db;
+const db = require('../../../util/firebase/firestore').db;
 
 export default async function (req, res) {
   const {songId} = req.query;
