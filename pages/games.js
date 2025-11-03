edba2413ad688f7cc294519cca7d8be10eb418da
Chandler Davidson <chandlermdavidson@gmail.com>
2025-11-03 16:29:01 -0500

    Add games page

---
+++ pages/games.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const games = [{
+    name: 'Hello Phaser',
+    icon: 'phaser-game',
+    loader: async () => {
+        const module = await import('template-bun/dist/phaser-game.es');
+        module.default('game-container');
+    }
+}];
+
+// Phaser recreates a canvas element on game start
+const removeExistingCanvas = () => document.getElementsByTagName('canvas')[0].remove();
+
+export default function Games() {
+    const [selected, setSelected] = useState(null);
+    const loadGame = (name, loader) => {
+        if (selected) {
+            removeExistingCanvas();
+        }
+
+        setSelected(name);
+        loader();
+    }
+
+    return (<>
+        <h1>Games!</h1>
+        <ul>
+            {games.map(({ name, icon, loader }) =>
+                <div>
+                    <h3>{name}</h3>
+                    <img key={name} src={`${icon}.png`} onClick={() => loadGame(name, loader)} />
+                </div>
+            )}
+        </ul>
+        <div id="app">
+            <div id="game-container"></div>
+        </div>
+    </>
+    )
+}
